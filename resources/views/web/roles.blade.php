232e1a2ef7718df85c2aacbedf2ec190e3131e09
hesamameri <hesam7991ameri@gmail.com>
2022-05-11 08:34:47 +0430

    vehicle-search-20/02
    a new route "vehicle-search" was added.
    blade files were changed.

--- resources/views/web/roles.blade.php
+++ resources/views/web/roles.blade.php
@@ -8,18 +8,11 @@
 
 
 @section('main')
-    <div class="container">
-
-        <section id="contact" class="margin-bottom-70">
-            <h4><i class="sl sl-icon-phone"></i> Contact Form</h4>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </section>
-    </div>
+<div id="utf_listing_overview" class="utf_listing_section">
+          <h3 class="utf_listing_headline_part margin-top-30 margin-bottom-30">Rules and Regulations</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in pulvinar neque. Nulla finibus lobortis pulvinar. Donec a consectetur nulla. Nulla posuere sapien vitae lectus suscipit, et pulvinar nisi tincidunt. Aliquam erat volutpat. Curabitur convallis fringilla diam sed aliquam.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in pulvinar neque. Nulla finibus lobortis pulvinar. Donec a consectetur nulla. Nulla posuere sapien vitae lectus suscipit, et pulvinar nisi tincidunt. Aliquam erat volutpat. Curabitur convallis fringilla diam sed aliquam. Sed tempor iaculis massa faucibus feugiat. In fermentum facilisis massa, a consequat purus viverra.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in pulvinar neque. Nulla finibus lobortis pulvinar. Donec a consectetur nulla. Nulla posuere sapien vitae lectus suscipit, et pulvinar nisi tincidunt. Aliquam erat volutpat. Curabitur convallis fringilla diam sed aliquam.</p>
 @endsection
 
 @section('script')
